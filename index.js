329277cec2c05060a515375708230db98fe9b5b4
Girisha Jhurry <Girisha.Jhurry@ceridian.com>
2021-10-01 13:49:41 +0400

    First commit

---
+++ index.js
@@ -0,0 +1,5 @@
+//console.log("hello typescript");
+var x = 5;
+function mysum(a, b) {
+    return a + b;
+}
---
+++ index.ts
@@ -0,0 +1,18 @@
+//console.log("hello typescript");
+
+// var x =5;
+
+// function mysum( a: number, b:number):number{
+//     return a+b;
+// }
+
+////////////////////////////////////////////
+
+export interface Person{
+    name: string;
+    age: number;
+}
+
+let human = {
+    
+}
